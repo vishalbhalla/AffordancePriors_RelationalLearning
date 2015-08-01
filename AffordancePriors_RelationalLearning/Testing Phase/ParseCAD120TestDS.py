d24f8aac85b64c368431279519c71efaccc97616
Vishal <vishal.a.bhalla@gmail.com>
2015-08-01 22:07:23 +0200

    Removed suffixes from words, stemmed & lemmatized them.

--- AffordancePriors_RelationalLearning/Testing Phase/ParseCAD120TestDS.py
+++ AffordancePriors_RelationalLearning/Testing Phase/ParseCAD120TestDS.py
@@ -5,6 +5,18 @@ import os
 import os.path
 from nltk.stem import PorterStemmer, WordNetLemmatizer
 
+
+## Stemming
+# Stemming is the process of reducing a word into its stem, i.e. its root form.
+# We are doing the stemming for comparison of our corpuses (train & test) nd it doesnt matter if we don't get a real world.
+stemmer = PorterStemmer()
+
+
+## Lemmatisation
+# The purpose of Lemmatisation is to group together different inflected forms of a word, called lemma.
+# It maps several words into one common root.
+lemmatiser = WordNetLemmatizer()
+
 #1204144410,stacking,subject1,1:box,2:box,3:box,4:box,
 #1204144736,stacking,subject1,1:bowl,2:bowl,3:bowl,
 #1204145234,stacking,subject1,1:plate,2:plate,3:plate,4:plate,5:plate,
@@ -47,7 +59,20 @@ for filePath in labelPath:
                     if Labeling[0] in activity[0]:
                         verbS = Labeling[3]
                         nounS = activity[2]
+
+                        strVerbNewS = ''
+                        if verbS.endswith('ing'):
+                            strVerbNewS = verbS[:-3] + 'e'
+                        if verbS.endswith('to'):
+                            strVerbNewS = verbS[:-2] + 'e'
+                        if verbS.endswith('able'):
+                            strVerbNewS = verbS[:-4] +'e'
+
+                        # Stem to find the root word.
+                        strStemmedVerbS = stemmer.stem(strVerbNewS)
+                        verbS = strStemmedVerbS
                         verbNounSVPair = 'nsubj(' + verbS + ', ' + nounS + ')' + '\n'
+
                         text_file.write(verbNounSVPair)    # + "\n"
                         objCount = 1
                         totalElem = len(activity)
@@ -57,26 +82,28 @@ for filePath in labelPath:
                                 verbO = Labeling[objCount + 3]
                                 nounwithIdx = activity[objCount + 2]
                                 nounO = nounwithIdx.split(':')[1]
+
+                                strVerbNewO = ''
+                                if verbO.endswith('ing'):
+                                    strVerbNewO = verbO[:-3] + 'e'
+                                if verbO.endswith('to'):
+                                    strVerbNewO = verbO[:-2] + 'e'
+                                if verbO.endswith('able'):
+                                    strVerbNewO = verbO[:-4] +'e'
+
+                                # Stem to find the root word.
+                                strStemmedVerbO = stemmer.stem(strVerbNewO)
+                                verbO = strStemmedVerbO
+
                                 verbNounVOPair = 'dobj(' + verbO + ', ' + nounO + ')' + '\n'
                                 text_file.write(verbNounVOPair)    # + "\n"
                             objCount = objCount + 1
 
-
 text_file.close()
 
 
-## Stemming
-# Stemming is the process of reducing a word into its stem, i.e. its root form.
-# We are doing the stemming for comparison of our corpuses (train & test) nd it doesnt matter if we don't get a real world.
-
-## Lemmatisation
-# The purpose of Lemmatisation is to group together different inflected forms of a word, called lemma.
-# It maps several words into one common root.
-
-#stemmer = PorterStemmer()
-lemmatiser = WordNetLemmatizer()
-
-text_fileLemma = open('CAD120SVOTuplesLemmatized.txt', 'w')
+#text_fileLemma = open('CAD120SVOTuplesLemmatized.txt', 'w')
+text_fileLemma = open('CAD120SVOTuplesStemmedLemmatized.txt', 'w')
 
 print 'Start the Lemmatization process to group together different words (lemma) to a common root for all grammar tuples'
 
@@ -113,3 +140,4 @@ for pair in SVOTuples:
 
 text_fileLemma.close()
 print 'Lemmatization process to group together different words (lemma) to a common root for all grammar tuples is completed'
+
