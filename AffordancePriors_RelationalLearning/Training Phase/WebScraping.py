51ef5d81bd8957f3f6ce6ef022764d53998adb81
Vishal <vishal.a.bhalla@gmail.com>
2015-08-03 13:27:17 +0200

    Web scraped WikiHow to extract text for training.

---
+++ AffordancePriors_RelationalLearning/Training Phase/WebScraping.py
@@ -0,0 +1,24 @@
+__author__ = 'vishal'
+
+
+from bs4 import BeautifulSoup, NavigableString
+from urllib2 import urlopen
+
+URLList = ["http://www.wikihow.com/Use-Citrus-Fruit-Peels-in-the-Home-and-Garden", "http://www.wikihow.com/Place-and-Finish-a-Concrete-Floor"]
+txtString = ''
+text_file = open('WebScrapingTrain.txt', 'w')
+
+for BASE_URL in URLList:
+    html = urlopen(BASE_URL).read()
+    soup = BeautifulSoup(html, "lxml")
+    sectionDiv = soup.findAll("div","step")
+    for item in sectionDiv:
+        listTags = item.findAll('li')
+        for childTag in listTags:
+            for grandchildTag in childTag:
+                if type(grandchildTag.string) == NavigableString:
+                    txtString += grandchildTag.string.encode('ascii', 'ignore').encode('utf-8')
+    txtString += "\n"+ "\n"+ "\n"
+
+text_file.write(txtString)    # + "\n"
+text_file.close()
