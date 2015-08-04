ab8071b4b9cc94e6938bfd1a0c33b2c4ac4d4996
Vishal <vishal.a.bhalla@gmail.com>
2015-08-04 13:45:22 +0200

    Remove special characters from nsubj & dobj Filtering.

--- AffordancePriors_RelationalLearning/Training Phase/FilterSVOTuples.py
+++ AffordancePriors_RelationalLearning/Training Phase/FilterSVOTuples.py
@@ -2,18 +2,13 @@ __author__ = 'vishal'
 
 
 text_file = open('FilteredSVOTuples.txt', 'w')
+print 'Start the Filtering process to retain only nusbj and dobj SD pairs from all grammar tuples'
 
-lsGrammarTuples=[]
 for line in open( 'ParsedGrammarTuples.txt', 'r' ).readlines():
-    lsGrammarTuples.append( line )
-
-    print 'Start the Filtering process to retain only nusbj and dobj SD pairs from all grammar tuples'
-
-    for pair in lsGrammarTuples:
-        if 'nsubj' in pair or 'dobj' in pair:
-            text_file.write(pair)    # + "\n"
+    if line.startswith('nsubj(') or line.startswith('dobj('):
+        if not '*' in line and not '%' in line and not '\'' in line:
+            text_file.write(line)    # + "\n"
 
 text_file.close()
-
 print 'Filtering to retain only nusbj and dobj SD pairs from all grammar tuples is completed'
 
