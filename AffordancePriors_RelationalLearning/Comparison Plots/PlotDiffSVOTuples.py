802261d245689e54450e268a7f2d6d188089faa5
Vishal <vishal.a.bhalla@gmail.com>
2015-08-12 18:14:59 +0200

    Compare training dataset against ground truth inferred probabilities.

--- AffordancePriors_RelationalLearning/Comparison Plots/PlotDiffSVOTuples.py
+++ AffordancePriors_RelationalLearning/Comparison Plots/PlotDiffSVOTuples.py
@@ -19,7 +19,7 @@ for pair in SVOTuplesTrain:
         newSVOTupleTrain.append((formattedSVOTupleTrain,probSVOTupleTrain))
 
 
-
+'''
 text_file = open('InferenceCAD120SVOTuples.txt', 'r')
 VerbNounTuplesTest = text_file.read()
 text_file.close()
@@ -33,6 +33,39 @@ for pair in SVOTuplesTest:
         formattedSVOTupleTest = str.translate(None, ' ')
         probSVOTupleTest = float(tupleTest[1])
         newSVOTupleTest.append((formattedSVOTupleTest,probSVOTupleTest))
+'''
+
+# Extract only ground truth tuples from test data for comparison or evaluation
+
+text_file = open('CAD120SVOTuples.txt', 'r')
+VerbNounTuplesTest = text_file.read()
+text_file.close()
+SVOTuplesTest = VerbNounTuplesTest.split('\n')
+
+# Select distinct elements
+CAD120SVOTuplesTest = list(set(SVOTuplesTest))
+# Remove empty strings
+CAD120SVOTuplesTest = filter(None, CAD120SVOTuplesTest)
+
+text_file = open('InferenceCAD120SVOTuples.txt', 'r')
+VerbNounTuplesTrain = text_file.read()
+text_file.close()
+SVOTupleTestInf = VerbNounTuplesTrain.split('\n')
+
+
+newSVOTupleTestInf = []
+listExists = []
+for pair in SVOTupleTestInf:
+    TupleTestInf = pair.split(' ')
+    if 'nsubj' in pair or 'dobj' in TupleTestInf[0]:
+        str = TupleTestInf[0].strip()
+        formattedSVOTupleTestInf = str.translate(None, ' ')
+        listExists = [formattedSVOTupleTestInf in [item for item in CAD120SVOTuplesTest]]
+        if listExists[0]:
+            probSVOTupleTestInf = float(TupleTestInf[1])
+            newSVOTupleTestInf.append((formattedSVOTupleTestInf,probSVOTupleTestInf))
+
+# End of extracting only ground truth tuples from test data for comparison or evaluation
 
 
 #newSVOTuplesTrainTest = []
@@ -43,7 +76,7 @@ newProbSVOTuplesTest = []
 for tupleTrain in newSVOTupleTrain:
      strTupleTrain = tupleTrain[0]
      probSVOTupleTrain = tupleTrain[1]
-     for tupleTest in newSVOTupleTest:
+     for tupleTest in newSVOTupleTestInf: #newSVOTupleTest: # Changed to compare only with ground truth tuples from test data.
         strTupleTest = tupleTest[0]
         probSVOTupleTest = tupleTest[1]
         if strTupleTrain == strTupleTest:
@@ -54,8 +87,58 @@ for tupleTrain in newSVOTupleTrain:
 
 
 # Plot the graph for the common tuples and their probabilities.
+# Scatter Plot
 SVOTupleNo = range(len(newSVOTuples))
 plt.xticks(SVOTupleNo, newSVOTuples)
-plt.scatter(SVOTupleNo,newProbSVOTuplesTrain,color='k')
-plt.scatter(SVOTupleNo,newProbSVOTuplesTest,color='g')
+train = plt.scatter(SVOTupleNo,newProbSVOTuplesTrain,color='k')
+test = plt.scatter(SVOTupleNo,newProbSVOTuplesTest,color='g')
+
+plt.title("Scatter Plot for Inferred SVO Tuple Probabilities of WikiHow Training Dataset vs Ground Truth of CAD 120 Test Dataset")
+plt.xlabel("SVO Tuples - Verb-Noun Pairs")
+plt.ylabel("Probability")
+plt.legend((train, test),
+           ('WikiHow Training Dataset', 'CAD 120 Test Dataset'),
+           scatterpoints=1,
+           loc='upper right',
+           ncol=1,
+           fontsize=8)
 plt.show()
+
+'''
+# Histogram
+totaNoBins = len(newSVOTuples)
+plt.hist(newProbSVOTuplesTrain, histtype='stepfilled', stacked=True, color='k', label='Inferred WikiHow Training dataset')
+plt.hist(newProbSVOTuplesTest, histtype='stepfilled', stacked=True, color='g', label='Ground truth of CAD 120 Test dataset')
+plt.title("Histogram of SVO Tuple Probabilities for Inferred WikiHow Training dataset vs ground truth of CAD 120 Test dataset")
+plt.xlabel("SVO Tuples - Verb-Noun Pairs")
+plt.ylabel("Probability")
+plt.legend()
+
+plt.show()
+
+# Learn about API authentication here: https://plot.ly/python/getting-started
+# Find your api_key here: https://plot.ly/settings/api
+
+import plotly.plotly as py
+from plotly.graph_objs import *
+
+import numpy as np
+x0 = np.random.randn(500)
+x1 = np.random.randn(500)+1
+
+trace1 = Histogram(
+    x=x0,
+    opacity=0.75
+)
+trace2 = Histogram(
+    x=x1,
+    opacity=0.75
+)
+data = Data([trace1, trace2])
+layout = Layout(
+    barmode='overlay'
+)
+fig = Figure(data=data, layout=layout)
+plot_url = py.plot(fig, filename='overlaid-histogram')
+
+'''
