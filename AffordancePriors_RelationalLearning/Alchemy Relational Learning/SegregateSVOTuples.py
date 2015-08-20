cbd649d98579fcbf95b9d0f3871b730460042533
Vishal <vishal.a.bhalla@gmail.com>
2015-08-20 14:13:52 +0200

    Segregate SVO Tuples to SV (nsubj) and VO (dobj) tuples in the Training Database.

---
+++ AffordancePriors_RelationalLearning/Alchemy Relational Learning/SegregateSVOTuples.py
@@ -0,0 +1,21 @@
+__author__ = 'vishal'
+
+SVTuples = []
+VOTuples = []
+for line in open( 'SVOTuples.db', 'r' ).readlines():
+    # Read verb-noun pairs in the file.
+    if line.startswith('nsubj('):
+        SVTuples.append(line)
+    elif line.startswith('dobj('):
+        VOTuples.append(line)
+
+# Write the filtered verb-noun individual type tuples from the SVO tuples to a file
+text_file = open('SVTuples.db', 'w')
+strSVtuples = ''.join([str(tup) for tup in SVTuples])
+text_file.write(strSVtuples)
+text_file.close()
+
+text_file = open('VOTuples.db', 'w')
+strVOtuples = ''.join([str(tup) for tup in VOTuples])
+text_file.write(strVOtuples)
+text_file.close()
