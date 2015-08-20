76393370c89004a2560bad37f4ec13840de3c81b
Vishal <vishal.a.bhalla@gmail.com>
2015-08-20 23:27:03 +0200

    Remove special characters during filtering.

--- AffordancePriors_RelationalLearning/Training Phase/FilterPositions.py
+++ AffordancePriors_RelationalLearning/Training Phase/FilterPositions.py
@@ -6,27 +6,35 @@ text_file = open('FormattedTuples.txt', 'w')
 
 print 'Start the Filtering process of positions on each verb-noun grammar tuple'
 for line in open( 'FilteredSVOTuples.txt', 'r' ).readlines():
-    str = line
-    formattedStr = str.translate(None, '-0123456789')
-    #Capitalize first letter of each word 
-    formattedStr = formattedStr.title()
-    formattedStr = formattedStr.replace('Nsubj', 'nsubj')
-    formattedStr = formattedStr.replace('Dobj', 'dobj')
-    
-    #Group Animate subjects higher up in the hierarchy for comparison.
-    formattedStr = formattedStr.replace('I)', 'Person)')
-    formattedStr = formattedStr.replace('You)', 'Person)')    
-    formattedStr = formattedStr.replace('He)', 'Person)')
-    formattedStr = formattedStr.replace('She)', 'Person)')
-    formattedStr = formattedStr.replace('Man)', 'Person)')
-    formattedStr = formattedStr.replace('Woman)', 'Person)')
-    formattedStr = formattedStr.replace('Men)', 'Person)')
-    formattedStr = formattedStr.replace('Women)', 'Person)')
-    formattedStr = formattedStr.replace('Child)', 'Person)')
-    formattedStr = formattedStr.replace('Children)', 'Person)')    
-    formattedStr = formattedStr.replace('They)', 'Person)')
 
-    text_file.write(formattedStr)    # + "\n"
+    # Remove position numbers from tuples
+    str = line.translate(None, '-0123456789')
+
+    # Remove special characters
+    str = str.translate(None, '=*/\~+-$%.!@#^')
+
+    # Check for empty nouns from dependency tuples or verb-noun pairs.
+    if not ', )' in str and not '(,' in str:
+
+        #Capitalize first letter of each word
+        formattedStr = str.title()
+        formattedStr = formattedStr.replace('Nsubj', 'nsubj')
+        formattedStr = formattedStr.replace('Dobj', 'dobj')
+    
+        #Group Animate subjects higher up in the hierarchy for comparison.
+        formattedStr = formattedStr.replace('I)', 'Person)')
+        formattedStr = formattedStr.replace('You)', 'Person)')
+        formattedStr = formattedStr.replace('He)', 'Person)')
+        formattedStr = formattedStr.replace('She)', 'Person)')
+        formattedStr = formattedStr.replace('Man)', 'Person)')
+        formattedStr = formattedStr.replace('Woman)', 'Person)')
+        formattedStr = formattedStr.replace('Men)', 'Person)')
+        formattedStr = formattedStr.replace('Women)', 'Person)')
+        formattedStr = formattedStr.replace('Child)', 'Person)')
+        formattedStr = formattedStr.replace('Children)', 'Person)')
+        formattedStr = formattedStr.replace('They)', 'Person)')
+
+        text_file.write(formattedStr)    # + "\n"
 
 text_file.close()
 print 'Filtering of positions for each verb-noun grammar tuple is completed'
