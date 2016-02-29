843fc84d66b101c126f169416dbe90ec104c30c7
Vishal <vishal.a.bhalla@gmail.com>
2016-02-29 01:12:06 +0100

    Count Active, Passive & Out of Vocabulary objects.

--- AffordancePriors_RelationalLearning/Comparison Plots/CompareObjPassivity.py
+++ AffordancePriors_RelationalLearning/Comparison Plots/CompareObjPassivity.py
@@ -75,3 +75,52 @@ for tupleObj in dobjTrain: # For each entity in the VO - MAP file [dobj(Verb, En
 
 print len(objPassivity)
 
+
+
+### Evaluation
+dobjCAD120Eval = []
+# Open the dobj CAD 120 file & Get Distinct dobj CAD 120 Tuples
+for line in open('CAD120SVOTuples.txt', 'r').readlines():
+    if 'dobj' in line: #dobj("Reach","Microwave")
+        tupleEval = line.split(',')
+        strObject = tupleEval[1].strip().replace('"','').replace(')','')
+        if not strObject in dobjCAD120Eval:
+            dobjCAD120Eval.append(strObject)
+
+print len(dobjCAD120Eval)
+
+
+
+# 4. For each entity in the VO - MAP file [dobj(Verb, Entity)] of CAD 120
+objCorrectClasify = []
+objMisClasify = []
+objBothClasify = []
+objOutOfVocab = []
+for obj in dobjCAD120Eval: # For each distinct entity in the CAD 120 dobj(Verb, Entity)] file
+     strObjExists = False
+     for tupleObj in objPassivity:
+         strObj = tupleObj[0]
+         strPassivity = tupleObj[1]
+         if strObj == obj:  # If dobj entity exists -> What is the corresponding label from Training
+             if strPassivity == 'Passive': # dobj is more or dobj entity doesn't exist in nsubj -> Passive
+                 objCorrectClasify.append(obj)
+             elif strPassivity == 'Active': # nsubj is more -> Active
+                 objMisClasify.append(obj)
+             elif strPassivity == 'Both': # dobj = nsubj -> Both
+                 objBothClasify.append(obj)
+
+             # dobj entity exists in CAD 120 dobj(Verb, Entity)]
+             strObjExists = True
+             break
+
+     if strObjExists == False: # dobj entity doesn't exist in dobj entity exists in CAD 120 dobj(Verb, Entity)] -> Out of Vocabulary
+         objOutOfVocab.append(obj)
+
+
+
+# 5. Draw Table with these 3 values for different number of links.
+# No of links vs (Passive correct, passive misclassified and out of vocabulary)
+print len(objCorrectClasify)
+print len(objMisClasify)
+print len(objBothClasify)
+print len(objOutOfVocab)
